fd6af1840efcd1dde4c3f2df95b6a4f09dd56d95
HowlinHank <HowlinHank@sbcglobal.net>
2015-05-20 23:46:48 -0700

    testing the cloning and updating

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,25 @@
+## cachematrix.R
+## Coursera R Programming course Week 3 assignment
+## HowlinHank
+## Copyright 2015, All Rights Reserved
+
+##------------------------------------------------------------------------------------------
+
+## makeCacheMatrix Library description
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
+##------------------------------------------------------------------------------------------
+
+## makeCacheMatrix(x= matrix())
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
+##------------------------------------------------------------------------------------------
 
 ## Write a short comment describing this function
 
